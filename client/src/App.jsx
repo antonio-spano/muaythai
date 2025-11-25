68b60456d699008d970fa428d6fdd4e9fff806fa
anto <antoniospano05@outlook.it>
2025-11-25 18:16:12 +0100

    First working version

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [status, setStatus] = useState("Server non contattato");
+  const [combo, setCombo] = useState("Premi il tasto per allenarti");
 
   const testConnection = async () => {
     try {
       // Chiama il backend sulla porta 5000
-      const response = await fetch('http://localhost:5000/'); 
+      const response = await fetch("http://localhost:5000/");
       const data = await response.json();
       setStatus(data.message);
       alert("Tutto funziona! Ora puoi scrivere il codice.");
@@ -15,19 +16,50 @@ function App() {
       console.error(error);
       setStatus("Errore: Backend non raggiungibile");
     }
-  }
+  };
+
+  const getNewCombo = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/combo");
+      const data = await response.json();
+
+      setCombo(data.combo);
+    } catch (error) {
+      console.error(error);
+      setCombo("Errore nel recupero combo");
+    }
+  };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Template Base Muay Thai</h1>
-      <div style={{ padding: '20px', border: '1px solid #333', margin: '20px' }}>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Muay Thai Combo Caller</h1>
+      <div
+        style={{ padding: "20px", border: "1px solid #333", margin: "20px" }}
+      >
         Status Backend: <strong>{status}</strong>
       </div>
-      <button onClick={testConnection} style={{ padding: '10px 20px', fontSize: '16px' }}>
-        TEST CONNESSIONE
+
+      <div
+        style={{ padding: "20px", border: "1px solid #333", margin: "20px" }}
+      >
+        Status Backend: <strong>{combo}</strong>
+      </div>
+
+      <button
+        onClick={testConnection}
+        style={{ padding: "10px 20px", fontSize: "16px" }}
+      >
+        Connect
+      </button>
+
+      <button
+        onClick={getNewCombo}
+        style={{ padding: "10px 20px", fontSize: "16px" }}
+      >
+        Generate Combo
       </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,56 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
-// --- SPAZIO PER LA TUA LOGICA ---
-// Qui scriverai l'algoritmo delle combo
-// --------------------------------
+const j = "jab";
+const c = "cross";
+const h = "hook";
+const u = "uppercut";
+const r = "roundhouse kick";
+const f = "front kick";
+const s = "side kick";
+const k = "knee strike";
+const e = "elbow strike";
+
+const move = [j, c, h, u, r, f, s, k, e];
+const side = ["left", "right"];
+const bodypart = ["low", "high", "body"];
+const combolen = 2;
+
+function generateCombo() {
+  let combo = "";
+
+  for (let i = 0; i < combolen; i++) {
+    let moverand = Math.floor(Math.random() * move.length);
+    let siderand = Math.floor(Math.random() * side.length);
+    let bodyrand = Math.floor(Math.random() * bodypart.length);
+
+    combo += side[siderand] + " " + bodypart[bodyrand] + " " + move[moverand];
+
+    if (i < combolen - 1) {
+      combo += " - ";
+    }
+  }
+
+  return combo;
+}
+
+app.get("/combo", (req, res) => {
+  const result = generateCombo();
+  console.log("Generata: ", result);
+  res.json({ combo: result });
+});
 
 // Test Route
-app.get('/', (req, res) => {
-    res.json({ message: 'Backend connesso con successo!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Backend connesso con successo!" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server attivo su http://localhost:${PORT}`);
+  console.log(`Server attivo su http://localhost:${PORT}`);
 });
