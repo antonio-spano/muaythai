44714e397f470cced6ce87214f4c97d42478b7f3
anto <antoniospano05@outlook.it>
2025-11-25 19:17:41 +0100

    Some polish work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [status, setStatus] = useState("Server non contattato");
   const [combo, setCombo] = useState("Premi il tasto per allenarti");
-
-  const testConnection = async () => {
-    try {
-      // Chiama il backend sulla porta 5000
-      const response = await fetch("http://localhost:5000/");
-      const data = await response.json();
-      setStatus(data.message);
-      alert("Tutto funziona! Ora puoi scrivere il codice.");
-    } catch (error) {
-      console.error(error);
-      setStatus("Errore: Backend non raggiungibile");
-    }
-  };
 
   const getNewCombo = async () => {
     try {
@@ -33,24 +19,12 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Muay Thai Combo Caller</h1>
-      <div
-        style={{ padding: "20px", border: "1px solid #333", margin: "20px" }}
-      >
-        Status Backend: <strong>{status}</strong>
-      </div>
 
       <div
         style={{ padding: "20px", border: "1px solid #333", margin: "20px" }}
       >
-        Status Backend: <strong>{combo}</strong>
+        <strong>{combo}</strong>
       </div>
-
-      <button
-        onClick={testConnection}
-        style={{ padding: "10px 20px", fontSize: "16px" }}
-      >
-        Connect
-      </button>
 
       <button
         onClick={getNewCombo}
